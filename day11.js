74fe9beb03bfe6ce6a9ad833f01e259ed838cfb0
rchaur1 <ritik.chaurasiya@morningstar.com>
2026-08-12 14:57:25 +0530

    Finding no of Unique element in an array.

---
+++ day11.js
@@ -0,0 +1,20 @@
+var removeDuplicates = function (nums) {
+  let pointer = 1;
+  let temp;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] != nums[pointer - 1]) {
+      temp = nums[i];
+      nums[i] = nums[pointer];
+      nums[pointer] = temp;
+      pointer++;
+    } else {
+      nums[i] = null;
+    }
+  }
+  return pointer;
+};
+
+let arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+let result = removeDuplicates(arr);
+console.log(result);
+console.log(arr);

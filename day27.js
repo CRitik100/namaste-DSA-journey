4d6029c2af3bea27ae52469e3fabb2ae905b4b11
rchaur1 <ritik.chaurasiya@morningstar.com>
2026-08-25 14:01:25 +0530

    Linear Search.

---
+++ day27.js
@@ -0,0 +1,17 @@
+/**
+ * Linear Search.
+ */
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let target = -9;
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (target == arr[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch(arr, target));
